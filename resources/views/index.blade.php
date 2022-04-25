f2a480c32a4fa5067515bc667b3876a33d5ec12e
JoseCarlosVC <9705@iespuertasdelcampo.es>
2022-04-25 20:44:02 +0200

    Primer modelo y vista

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" sizes="192x192" href="{{ asset('./favicon.ico') }}" type="image/x-icon">
+    <title>Munchio</title>
+</head>
+
+<body>
+    <header>
+        <a href="{{ url('/') }}"><img src="{{ asset('images/logo.png') }}" alt="Munchio"></a>
+        <nav>
+
+        </nav>
+    </header>
+    <h1>Munchio</h1>
+</body>
+
+</html>
