764eed022b73916470df0f30925e4348a1c68691
JoseCarlosVC <9705@iespuertasdelcampo.es>
2022-04-30 20:58:00 +0200

    Terminado el index mobile

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -232,7 +232,7 @@
     <div class="container-xxl py-5">
         <div class="container">
             <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
-                <h1 class="text-primary">Especialidades del día</h1>
+                <h1 class="display-2 text-black animated slideInDown">Especialidades del día</h1>
             </div>
             <div class="row g-4">
                 <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
